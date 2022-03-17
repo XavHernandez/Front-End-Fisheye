aa0248b1c6eaf12c7f5191c18bebc777f0151ba6
Xavier Hernandez <xhernan.martinez@gmail.com>
2022-03-17 21:34:13 +0100

    Add formdata handler + modal close with keyboard

--- scripts/features/modal.js
+++ scripts/features/modal.js
@@ -8,8 +8,8 @@ export default class Modal {
     this.photographer = photographer;
     this.photographerBadge = document.querySelector(".name");
     this.contactButton = document.querySelector(".contact_button");
-    this.modalButton = document.querySelector(".contact_button_dialog");
-    this.closeButton = document.querySelector(".modal header img");
+    this.submitButton = document.querySelector(".contact_button_dialog");
+    this.closeButton = document.querySelector(".close_modal");
     this.modalWrapper = document.querySelector(".contact_modal");
     this.modal = document.querySelector(".modal");
     this.main = document.getElementById("main");
@@ -17,6 +17,16 @@ export default class Modal {
 
   toggleVisibility() {
     this.modalWrapper.classList.toggle("visible");
+  }
+
+  handleFormData() {
+    const formInputs = Array.from(
+      document.querySelectorAll("#contactForm input")
+    );
+    formInputs.push(document.querySelector("#contactForm textarea"));
+    let formData = {};
+    formInputs.forEach((form) => (formData[form.name] = form.value));
+    console.log("Sending JSON data:", JSON.stringify(formData));
   }
 
   displayModal() {
@@ -33,6 +43,23 @@ export default class Modal {
       this.main.setAttribute("aria-hidden", false);
       this.modalWrapper.setAttribute("aria-hidden", true);
       this.toggleVisibility();
+    });
+    this.submitButton.addEventListener("click", (event) => {
+      event.preventDefault();
+      this.handleFormData();
+      this.main.setAttribute("aria-hidden", false);
+      this.modalWrapper.setAttribute("aria-hidden", true);
+      this.toggleVisibility();
+    });
+    document.addEventListener("keyup", (event) => {
+      if (
+        event.key === "Escape" &&
+        this.modalWrapper.classList.contains("visible")
+      ) {
+        this.main.setAttribute("aria-hidden", false);
+        this.modalWrapper.setAttribute("aria-hidden", true);
+        this.toggleVisibility();
+      }
     });
   }
 
